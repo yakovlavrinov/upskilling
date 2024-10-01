3313ff0b98758a2cb076a17576937350685f2674
Yakov <lawrinov.yackov@yandex.ru>
2024-10-01 21:01:29 +0300

    adding webpack-dev-server and env

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,9 +3,10 @@ import { BuildOptions } from "./types/config";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
 import { buildPlugins } from "./buildPlugins";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const { mode, paths } = options;
+    const { mode, paths, isDev } = options;
     return {
         mode: mode,
         entry: paths.entry,
@@ -19,5 +20,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         },
         resolve: buildResolvers(),
         plugins: buildPlugins(options),
+        devtool: isDev ? "inline-source-map" : undefined,
+        devServer: isDev? buildDevServer(options) : undefined,
     };
 }
